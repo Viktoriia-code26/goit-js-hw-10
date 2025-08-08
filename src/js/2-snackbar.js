8ede5a8b465f726b2de498c67bffae37d3bac0b3
Viktoriia P. <pohrebniakvika@gmail.com>
2025-08-08 13:14:41 +0200

    all files included

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,39 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delay = parseInt(this.elements.delay.value);
+  const state = this.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+  promise
+    .then(delay => {
+      console.log(`✅ Fulfilled promise in ${delay}ms`);
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      console.log(`❌ Rejected promise in ${delay}ms`);
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+  this.reset();
+});
